e5612c6e668cb5ac406fac25f5f577b3ad23392f
george <george.stemmann@gmail.com>
2022-07-20 03:48:05 -0700

    added tests and bonus tests2

--- addCommas.test.js
+++ addCommas.test.js
@@ -4,23 +4,24 @@ const addCommas = require('./addCommas');
 describe("#addCommas", () => {
   test("it is a function", () => {
     expect(typeof addCommas).toBe("function");
-  });
+        }
+        );
   test('it returns "1,000" when passed 1000', () => {
     expect(addCommas(1000)).toBe("1,000");
-  }
-    );
+        }
+        );
     test('it returns "1,000,000" when passed 1000000', () => {
         expect(addCommas(1000000)).toBe("1,000,000");
         }
         );
-        test('it returns "1,000,000,000" when passed 1000000000', () => {
-            expect(addCommas(1000000000)).toBe("1,000,000,000");
-            }
-            );
-            test('it returns "1,000,000,000,000" when passed 1000000000000', () => {
-                expect(addCommas(1000000000000)).toBe("1,000,000,000,000");
-                }
-                );
+    test('it returns "1,000,000,000" when passed 1000000000', () => {
+        expect(addCommas(1000000000)).toBe("1,000,000,000");
+        }
+        );
+    test('it returns "1,000,000,000,000" when passed 1000000000000', () => {
+        expect(addCommas(1000000000000)).toBe("1,000,000,000,000");
+        }
+        );
     test('random cases', () => {
         expect(addCommas(1234)).toBe('1,234');
         expect(addCommas(12345)).toBe('12,345');
